d44871203b040e3b35cecd48940376914350d3b3
Rafael Nobre <nobre84@gmail.com>
2020-06-01 07:13:13 -0300

    Removes LoadingView

---
+++ iPSChallenge/View/ActivityIndicator.swift
@@ -0,0 +1,24 @@
+//
+//  LoadingView.swift
+//  SwiftIssues
+//
+//  Created by Rafael Nobre on 16/05/20.
+//  Copyright © 2020 Rafael Nobre. All rights reserved.
+//
+
+import SwiftUI
+import UIKit
+
+struct ActivityIndicator: UIViewRepresentable {
+
+    @Binding var isAnimating: Bool
+    let style: UIActivityIndicatorView.Style
+
+    func makeUIView(context: UIViewRepresentableContext<ActivityIndicator>) -> UIActivityIndicatorView {
+        return UIActivityIndicatorView(style: style)
+    }
+
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: UIViewRepresentableContext<ActivityIndicator>) {
+        isAnimating ? uiView.startAnimating() : uiView.stopAnimating()
+    }
+}
